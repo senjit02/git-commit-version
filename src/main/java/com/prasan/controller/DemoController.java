280c94b544dbbdeadc53e85c9793b2c34a8c57b4
senjit02 <m.senjit@gmail.com>
2018-11-07 15:56:43 +0530

    modified pom

--- src/main/java/com/prasan/controller/DemoController.java
+++ src/main/java/com/prasan/controller/DemoController.java
@@ -1,5 +1,11 @@
 package com.prasan.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+//import org.omg.CORBA.portable.InputStream;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +40,32 @@ public class DemoController {
 	public String getVersion()
 	{
 		logger.debug("Inside getVersion");
-		 return "Version 1.0-SNAPSHOT";
+		 //return "Version 1.0-SNAPSHOT";
+		return readGitProperties();
+	}
+	
+	
+	
+	private String readGitProperties() {
+	    ClassLoader classLoader = getClass().getClassLoader();
+	    InputStream inputStream = classLoader.getResourceAsStream("git.properties");
+	    try {
+	        return readFromInputStream(inputStream);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return "Version information could not be retrieved";
+	    }
+	}
+	private String readFromInputStream(InputStream inputStream)
+	throws IOException {
+	    StringBuilder resultStringBuilder = new StringBuilder();
+	    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+	        String line;
+	        while ((line = br.readLine()) != null) {
+	            resultStringBuilder.append(line).append("\n");
+	        }
+	    }
+	    return resultStringBuilder.toString();
 	}
 
 }
